270b7bcefc7f5b5173175fa3f2b56d5689178fdb
anyelitacaradebolita <166782620+anyelitacaradebolita@users.noreply.github.com>
2026-06-21 00:17:17 -0600

    Implementación del módulo de calificaciones: se agregó la gestión completa de notas permitiendo registrar, editar y eliminar calificaciones de estudiantes matriculados. Se incorporaron validaciones para verificar que la nota sea numérica y esté en el rango de 0 a 100, además del control de matrícula previa antes de asignar una calificación. Se desarrolló el cálculo de promedios por estudiante y por curso, la determinación automática del estado aprobado o reprobado según la nota obtenida y la visualización de los registros en JTable, manteniendo la estructura por capas y el manejo de excepciones personalizadas del sistema

---
+++ src/proyecto_swing/excepciones/NotaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_swing.excepciones;
+
+/**
+ *
+ * @author Anyel
+ */
+public class NotaException extends Exception{
+       public NotaException(String mensaje) {
+        super(mensaje);
+    }
+}
